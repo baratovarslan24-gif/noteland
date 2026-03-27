540b97ae9d624cc464e8634c7ea89c9149fc0b44
Arslan Baratov <baratovarslan24@gmail.com>
2026-03-27 16:34:02 +0600

    add categories

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,6 +7,7 @@ import { Users } from './collections/Users'
 import { Media } from './collections/Media'
 import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import { ru } from '@payloadcms/translations/languages/ru'
+import { Categories } from './collections/Categories'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -18,7 +19,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
+  collections: [Users, Media, Categories],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
