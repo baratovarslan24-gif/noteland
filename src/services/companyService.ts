033f492d30a30733e33e46da84428520e49ebf2a
Arslan Baratov <baratovarslan24@gmail.com>
2026-04-05 13:33:26 +0600

    fix code

--- src/services/companyService.ts
+++ src/services/companyService.ts
@@ -1,7 +1,8 @@
 import { cache } from 'react'
 
 export const getCompany = cache(async () => {
-  const res = await fetch(`${process.env.API_URL}/globals/website`)
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+  const res = await fetch(`${baseUrl}/api/globals/website`)
 
   if (!res.ok) {
     throw new Error('Ошибка загрузки данных')
