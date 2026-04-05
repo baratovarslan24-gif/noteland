c91a1e1e9fcdd542c1f51a62c68e087a781bb6a5
Arslan Baratov <baratovarslan24@gmail.com>
2026-04-05 13:46:53 +0600

    fix code

--- src/services/companyService.ts
+++ src/services/companyService.ts
@@ -1,8 +1,15 @@
 import { cache } from 'react'
 
 export const getCompany = cache(async () => {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
-  const res = await fetch(`${baseUrl}/api/globals/website`)
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL
+
+  if (!baseUrl) {
+    throw new Error('NEXT_PUBLIC_SITE_URL is not defined')
+  }
+
+  const res = await fetch(`${baseUrl}/api/globals/website`, {
+    cache: 'no-store',
+  })
 
   if (!res.ok) {
     throw new Error('Ошибка загрузки данных')
